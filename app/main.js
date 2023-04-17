9eff48d3c3abd7576ff8b7fdd5c9d22e002778a3
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-04-17 12:09:55 -0600

    1

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
-console.log('hellow world')
+const { app, BrowserWindow } = require('electron');
+const { contextIsolated } = require('process');
+
+let mainWindow = null;
+
+
+
+app.on('ready', () => {
+    mainWindows = new BrowserWindow({
+        // webPreferences:{
+        //     nodeIntegration: true,
+        //     contextIsolated: false
+        // }
+    })
+    mainWindows.loadFile(__dirname + '/index.html')
+    
+})
+
+console.log("Iniciando")
